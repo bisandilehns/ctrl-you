a013a34d140fff878ac767e3364ca46d610d8bb8
bisandilehns <stu25318@ashs.school.nz>
2025-07-12 01:52:15 +1200

    Finished login page with working Flask backend flask setup

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
-
+@app.route("/login", methods=["GET", "POST"])
+def login ():
+    if request.method == "POST":
+        username = request.form ["username"]
+        password = request.form ["password"]
+        print("username:",username)
+        print("password", password)
+    return render_template("login.html")
+app.run(debug=True)
